13a08ef73c4daa0780a42eaa067a00c9048bfb54
Gustavo Malcotti <gustavo@malcotti.com>
2023-05-19 20:59:58 -0300

    1

---
+++ clase_06/bootstrap/js/scripts.js
@@ -0,0 +1,21 @@
+let texto = "152";
+
+let a = 'HOLA';
+let b = "MUNDO";
+
+let c = a + "-" + b;
+
+let existe = true; // false
+
+//texto = "NO doy mas";
+
+let btn1 = document.getElementById('btn1');
+
+btn1.addEventListener("click", () => {
+    //alert(texto);
+    console.log(c);
+    console.log(typeof existe);
+})
+
+
+
